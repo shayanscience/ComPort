95e5095c198685f09987306975b1cfc81aab11d8
shayansceince <shayancomeon@gmail.com>
2026-02-10 16:49:06 -0800

    Added function, variables, change in implementation: - Added 'write_message()' funciton. Host message is passed to this function. - Added 'buffer_write', 'bytesWritten' variables to store user input, and record number of bytes written to target, respectively. - Added 'while' loop for as long as user want to talk to Target. Write, and read associated functions reside in this loop now.

--- ComPort/main.cpp
+++ ComPort/main.cpp
@@ -31,9 +31,8 @@
 /// This function has default serial port settings.
 ///		BuadRate = 9600
 ///		ByteSize = 8
-///		DCBlength = sizeof(DCB)
+/// 	Parity = NOPARITY
 ///		StopBits = ONESTOPBIT
-///		Parity = NOPARITY
 /// </summary>
 /// <param name="dcb"></param>
 void serialPortSetup(
@@ -66,19 +65,39 @@ void serialPortSetup(DCB* dcb) {		/// Function added
 }
 
 
+/// <summary>
+/// write_message(HANDLE hfile, std::string& msg, DWORD* bytesWritten) 
+/// hfile -> File handler for the created file. Appropriate COMPORT is assigned in this handler.
+/// msg	  -> Takes user input.
+/// bytesWritten	-> Populates the number of bytes written in 'bytesWritten'.
+/// </summary>
+/// <param name="hfile"></param>
+/// <param name="msg"></param>
+/// <param name="bytesWritten"></param>
+/// <returns></returns>
+bool write_message(HANDLE hfile, std::string& msg, DWORD* bytesWritten) {
+	return WriteFile(
+		hfile,
+		msg.c_str(),	// Retrieves the string.
+		msg.length(),	// Gets length of the string.
+		bytesWritten,
+		NULL
+	);
+}
 
 int main() {
 	wchar_t devices[65535];		// wchar_t (wide character type) 16-bit, has capacity to stor 65535 devices.
-	char buffer[32] = { 0 };	// buffer is used to receive bytes from the other device - can receive up to 32bytes of information.
+	char read_buffer[32] = { 0 };	// buffer is used to receive bytes from the other device - can receive up to 32bytes of information.
 	COMMTIMEOUTS timeOut;		// API structure to configure the timeout parameters.
 	DCB dcb;					// dcb(data control block) is a API structure for configuration of the serial communication. It contains info such as: baudRate, Parity, and etc.
 	COMSTAT comStat = { 0 };	// COMSTAT is an API structure that is used to check how many bytes are available on the slave device.
 	DWORD errors = 0;			// Used in ClearComStat() function 
 	DWORD bytesRead = 0;		// Used in ReadFile() so that it shows how many bytes have been read by the system.
-	DWORD bytesWritten;			/// Added variable used in write file.
-	DWORD start_time = GetTickCount64();
-	DWORD timeout_limit = 2000;
-	bool time_out = false;
+	DWORD bytesWritten;			// This variabl stores number of bytes written to the target by the host.
+	DWORD start_time = GetTickCount64();	// Getting current tick.
+	DWORD timeout_limit = 2000;				// Conditional variable
+	bool time_out = false;					// Time out flag.
+	std::string write_buffer;				// Stores user input.
 
 	// Function below will look into all devices connected to the system and return their size (number).
 	// This fills the 'devices' variable with port names.
@@ -120,7 +139,7 @@ int main() {
 		std::cout << "COM port is opened" << std::endl;
 		bool device_control_Getstate = GetCommState(hfile, &dcb);
 
-		if(device_control_Getstate == 0){ // if getting the port configuration failed, output the error value.
+		if (device_control_Getstate == 0) { // if getting the port configuration failed, output the error value.
 			std::cout << "Error: " << GetLastError() << std::endl;
 			return 1;
 		}
@@ -128,7 +147,7 @@ int main() {
 
 		serialPortSetup(&dcb, 9600, 8, NOPARITY, ONESTOPBIT);
 		//serialPortSetup(&dcb);
-
+		/// Commented function above is used for testing 
 
 		// SetCommState will set above values to the port configuration.
 		bool device_control_SetState = SetCommState(hfile, &dcb);
@@ -144,7 +163,7 @@ int main() {
 
 		// Reading communication timeouts.
 		bool comm_timeOut_Getstate = GetCommTimeouts(hfile, &timeOut);
-		if (comm_timeOut_Getstate ==0) {
+		if (comm_timeOut_Getstate == 0) {
 			std::cout << "Error: " << GetLastError() << std::endl;
 			return 1;
 		}
@@ -169,84 +188,86 @@ int main() {
 		std::cout << "Time Out Has successfully been set!" << std::endl;
 
 
-		// We attempt to write the message 'help'. It is worthwhile to mention that our target device is set to listent
-		// to 'help' once it receives it, it will respond appropriately.
-		bool write_status = WriteFile(
-			hfile,
-			"help\n",
-			5,
-			&bytesWritten,
-			NULL
-		);
-
-		// Below check the return value from 'WriteFile' whether or not the write was successful.
-		if (write_status == 0) {
-			std::cout << "Error: " << GetLastError() << std::endl;
-			return 1;
-		}
-		else {
-			std::cout << "Message successfully written!" <<  std::endl;
-		}
-
-		bool comm_error_status;
-
-		// 'ClearCommError' it is a windows API that reports Com-port errors and updates COMSTAT structure.
-		/*	Note on COMSTAT Structure:
-		*							This structure contains a stats reporting number of bytes waiting. Without calling
-		*							this function the 'cblnQue' won't get updated.
-		*	Below we wait for as long as 'cbInQue' remains zero.
-		*	It indicates that there are bytes waiting to be read if it is not zero.
-		*/
-		do {
-			comm_error_status = ClearCommError(
-				hfile,
-				&errors,
-				&comStat
-			);
-			if (GetTickCount64() - start_time > timeout_limit) {
-				time_out = true;
-				break;
+		// While loop below, repeats the processes. It waits for the user input.
+		//	Depending on how many options available, it will respond accordingly until user envoke "q" which
+		//	stands for quit, then the process will end and the system closes the port.
+		while(true){
+			std::cin >> write_buffer;
+			// We attempt to write the message 'help'. It is worthwhile to mention that our target device is set to listent
+			// to 'help' once it receives it, it will respond appropriately.
+			bool write_status = write_message(hfile, write_buffer, &bytesWritten);
+			// Below check the return value from 'WriteFile' whether or not the write was successful.
+			if (write_status == 0) {
+				std::cout << "Error: " << GetLastError() << std::endl;
+				return 1;
 			}
-			//std::cout << "comm error: " << errors << std::endl;
-		} while (!comStat.cbInQue);
+			else {
+				std::cout << "Message successfully written!" << std::endl;
+			}
 
-		if (time_out) {
-			std::cout << "Timeout: No response from target!" << std::endl;
-		}
+			bool comm_error_status;
 
-		// If we get zero from the function 'ClearCommError()' it indicates there has been an issue.
-		if (comm_error_status == 0) {
-			std::cout << "Error: " << GetLastError() << std::endl;
-		}
-		else {// If we succeed we can check how many bytes are there to be read.
-			std::cout << comStat.cbInQue <<" Bytes waiting to be read!" << std::endl;
-		}
+			// 'ClearCommError' it is a windows API that reports Com-port errors and updates COMSTAT structure.
+			/*	Note on COMSTAT Structure:
+			*							This structure contains a stats reporting number of bytes waiting. Without calling
+			*							this function the 'cblnQue' won't get updated.
+			*	Below we wait for as long as 'cbInQue' remains zero.
+			*	It indicates that there are bytes waiting to be read if it is not zero.
+			*/
+			do {
+				comm_error_status = ClearCommError(
+					hfile,
+					&errors,
+					&comStat
+				);
+				if (GetTickCount64() - start_time > timeout_limit) {
+					time_out = true;
+					break;
+				}
+				//std::cout << "comm error: " << errors << std::endl;
+			} while (!comStat.cbInQue);
+
+			if (time_out) {
+				std::cout << "Timeout: No response from target!" << std::endl;
+			}
+
+			// If we get zero from the function 'ClearCommError()' it indicates there has been an issue.
+			if (comm_error_status == 0) {
+				std::cout << "Error: " << GetLastError() << std::endl;
+			}
+			else {// If we succeed we can check how many bytes are there to be read.
+				std::cout << comStat.cbInQue << " Bytes waiting to be read!" << std::endl;
+			}
 
 
 			// If there exist any bytes waiting to be read. We begin to read the information, and store it in 'buffer'.
 			bool read_status = ReadFile(
 				hfile,
-				buffer,
-				sizeof(buffer),
+				read_buffer,
+				sizeof(read_buffer),
 				&bytesRead,
 				NULL
 			);
 
 			// We need to use null terminator to display the end of the message.
-			buffer[bytesRead] = '\0';
+			read_buffer[bytesRead] = '\0';
 
-		// Checking the 'read_status' for debugging purposes.
-		if (read_status == 0) {
-			std::cout << "Error: " << GetLastError() << std::endl;
-		}
-		else { // Outputting message showing that the read process was successful, and the message will be shown respectively.
-			std::cout << "Message successfully has been received!" << std::endl;
-			std::cout << "\nReceived Message: " << buffer << std::endl;
-		}
+			// Checking the 'read_status' for debugging purposes.
+			if (read_status == 0) {
+				std::cout << "Error: " << GetLastError() << std::endl;
+			}
+			else { // Outputting message showing that the read process was successful, and the message will be shown respectively.
+				std::cout << "Message successfully has been received!" << std::endl;
+				std::cout << "\nReceived Message: " << read_buffer << std::endl;
+			}
 
-		// We close the communication line by envoking 'CloseHandle(handle file)'.
-		CloseHandle(hfile);
-		std::cout << "Handle is closed" << std::endl;
+			// We close the communication line by envoking 'CloseHandle(handle file)'.
+			if (write_buffer == "q") {
+				CloseHandle(hfile);
+				std::cout << "Handle is closed" << std::endl;
+				return 1;
+			}
+		}
 	}
 
 	return 0;
